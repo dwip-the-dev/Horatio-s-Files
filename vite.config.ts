493739ba04c544034ddfdbd25740013c20f3029b
Dwip Biswas <dwipbiswas@yahoo.com>
2026-06-13 17:57:01 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "node:url";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    tanstackStart(),
+    react(),
+    tailwindcss(),
+    nitro(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  environments: {
+    ssr: {
+      build: {
+        rollupOptions: {
+          input: "./src/server.ts",
+        },
+      },
+    },
+  },
+});
